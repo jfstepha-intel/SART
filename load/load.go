e4d9da3e4015ad7f3570833b5ce9d9c078e96714
ngeorge3 <nishant.george@intel.com>
2018-05-25 11:22:48 -0700

    Include logic from markprim at the end of loading. No reason not to run this along with load.

--- load/load.go
+++ load/load.go
@@ -9,12 +9,14 @@ import (
     "sync"
 
     "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 
     "sart/parse"
     "sart/rtl"
+    "sart/set"
 )
 
-func worker(wg *sync.WaitGroup, jobs <-chan string) {
+func parseWorker(wg *sync.WaitGroup, jobs <-chan string) {
     for path := range jobs {
         file, err := os.Open(path)
         if err != nil {
@@ -28,8 +30,39 @@ func worker(wg *sync.WaitGroup, jobs <-chan string) {
     wg.Done()
 }
 
+func updateWorker(wg *sync.WaitGroup, jobs <-chan string) {
+    sess := session.Copy()
+    inst := sess.DB("sart").C(cache+"_insts")
+    conn := sess.DB("sart").C(cache+"_conns")
+    for itype := range jobs {
+        _, err := inst.UpdateAll(
+            bson.M{"type": itype}, // Selector
+            bson.M{"$set": 
+                bson.M{"isprim": true},
+            },
+        )
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        _, err = conn.UpdateAll(
+            bson.M{"itype": itype}, // Selector
+            bson.M{"$set": 
+                bson.M{"isprim": true},
+            },
+        )
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+    wg.Done()
+}
+
+var session *mgo.Session
+var cache string
+
 func main() {
-    var path, server, cache string 
+    var path, server string 
     var threads int
 
     flag.StringVar(&path,   "path",    "",          "path to folder with netlist files")
@@ -46,7 +79,11 @@ func main() {
 
     log.SetFlags(log.Lshortfile)
 
-    session, err := mgo.Dial(server)
+    // Connect to MongoDB //////////////////////////////////////////////////////
+
+    var err error
+
+    session, err = mgo.Dial(server)
     if err != nil {
         log.Fatal(err)
     }
@@ -54,18 +91,22 @@ func main() {
 
     log.SetOutput(os.Stdout)
 
+    // Setup inputs, waitgroup and worker threads //////////////////////////////
+
     files, err := ioutil.ReadDir(path)
     if err != nil {
         log.Fatal(err)
     }
 
-    var wg sync.WaitGroup
-    jobs := make(chan string, 100)
+    var parsewg sync.WaitGroup
+    parsejobs  := make(chan string, 100)
 
     for i := 0; i < threads; i++ {
-        go worker(&wg, jobs)
-        wg.Add(1)
+        go parseWorker(&parsewg, parsejobs)
+        parsewg.Add(1)
     }
+
+    // Loop over files and add to parsers pool /////////////////////////////////
 
     count := 0
     total := len(files)
@@ -73,20 +114,79 @@ func main() {
         filename := file.Name()
         count++
         
-        if !strings.HasSuffix(filename, ".v") && !strings.HasSuffix(filename, ".vg"){
+        if  !strings.HasSuffix(filename, ".v") &&
+            !strings.HasSuffix(filename, ".vg") {
             continue
         }
 
         fpath := path + "/" + filename
-        jobs <- fpath
+        parsejobs <- fpath
 
         log.Printf("(%d/%d) %s", count, total, filename)
     }
 
     // No more parse jobs
-    close(jobs)
-    wg.Wait()
+    close(parsejobs)
+    parsewg.Wait()
 
     rtl.DoneMgo() // Signal no more mongo insert jobs
     rtl.WaitMgo() // Wait for all insert jobs to complete
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    // At this point all available information in the input netlists have been
+    // parsed, sliced and diced into wires, insts and conns collections in the
+    // mongo database. Next we need to mark all the instantiations for which a
+    // module definition was not found as primitives.
+    ////////////////////////////////////////////////////////////////////////////
+
+    // These are the modules for which a definition is available -- defined
+    // modules
+    var defnmodules []interface{}
+    err = session.DB("sart").C(cache + "_nodes").Find(nil).Distinct("module", &defnmodules)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // These are modules that have been instantiated at least once --
+    // instantiated modules
+    var instmodules []interface{}
+    err = session.DB("sart").C(cache + "_insts").Find(nil).Distinct("type", &instmodules)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Create sets out of these lists
+    defn := set.New(defnmodules...)
+    inst := set.New(instmodules...)
+
+    // Setup worker pool for update queries ////////////////////////////////////
+
+    var updatewg sync.WaitGroup
+    updatejobs := make(chan string, 100)
+
+    for i := 0; i < threads; i++ {
+        go updateWorker(&updatewg, updatejobs)
+        updatewg.Add(1)
+    }
+
+    // Instantiated, but not defined implies primitives or EBBs. Either way
+    // they'll have to be treated as primitives as there is no information on
+    // them to go by.
+
+    prims := inst.Not(defn).Sort()
+    total  = len(prims)
+    count  = 0
+
+    // Loop over each primitive that was found and add to the updaters pool
+    for _, prim := range prims {
+        updatejobs <- prim
+        count ++
+        log.Printf("prim: (%d/%d) %s", count, total, prim)
+    }
+    close(updatejobs)
+
+    updatewg.Wait()
+
+    ////////////////////////////////////////////////////////////////////////////
 }
