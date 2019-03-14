e4c24e7ea9b1bc38f9ffa18f4de147ec21126114
ngeorge3 <nishant.george@intel.com>
2019-03-14 12:40:27 -0700

    Switch from csv to json for ace structs file format.

--- ace/ace.go
+++ ace/ace.go
@@ -1,72 +1,30 @@
 package ace
 
 import (
-	"bufio"
+	"encoding/json"
 	"io"
 	"log"
-	"regexp"
-	"strconv"
-	"strings"
 )
 
 func init() {
 	log.SetFlags(log.Lshortfile)
 }
 
+type Regex struct {
+	Module string `json:"module"`
+	Name   string `json:"name"`
+}
+
 type AceStruct struct {
-	Field string
-	Regex string
-	Rpavf float64
-	Wpavf float64
+	Selector Regex   `json:"sel"`
+	Rpavf    float64 `json:"rpavf"`
+	Wpavf    float64 `json:"wpavf"`
 }
 
 func Load(reader io.Reader) (acestructs []AceStruct) {
-	scanner := bufio.NewScanner(reader)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		line = strings.TrimSpace(line)
-
-		// Skip whitespace lines
-		if line == "" {
-			continue
-		}
-
-		// Skip comments
-		if strings.HasPrefix(line, "#") || strings.HasPrefix(line, "//") {
-			continue
-		}
-
-		// Split by comma and expect to find exactly 3 parts
-		parts := strings.Split(line, ",")
-		if len(parts) != 4 {
-			log.Fatal("Expecting 4 parts in ACE struct", line, parts)
-		}
-
-		field := strings.TrimSpace(parts[0])
-		regex := strings.TrimSpace(parts[1])
-
-		// Do a basic check to see that the regex is valid -- i.e. at least
-		// compilable by Go
-		_, err := regexp.Compile(regex)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Convert the number strings into float type before passing on
-		rpace, err := strconv.ParseFloat(strings.TrimSpace(parts[2]), 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		wpace, err := strconv.ParseFloat(strings.TrimSpace(parts[3]), 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		acestruct := AceStruct{field, regex, rpace, wpace}
-		acestructs = append(acestructs, acestruct)
+	err := json.NewDecoder(reader).Decode(&acestructs)
+	if err != nil {
+		log.Fatal(err)
 	}
-
 	return
 }
--- ace/ace_test.go
+++ ace/ace_test.go
@@ -5,51 +5,59 @@ import (
 	"testing"
 )
 
-func TestWhitespace(t *testing.T) {
-	str := `
-
-  
-	`
-	a := Load(strings.NewReader(str))
-	if len(a) != 0 {
-		t.Errorf("Expecting to get a slice with 0 AceStruct. Got %d", len(a))
-	}
-}
-
-func TestPoundComments(t *testing.T) {
-	a := Load(strings.NewReader("# Comment"))
-	if len(a) != 0 {
-		t.Errorf("Expecting to get a slice with 0 AceStruct. Got %d", len(a))
-	}
-}
-
-func TestSlashComments(t *testing.T) {
-	a := Load(strings.NewReader("// Comment"))
-	if len(a) != 0 {
-		t.Errorf("Expecting to get a slice with 0 AceStruct. Got %d", len(a))
-	}
-}
-
-func TestLineWithSpace(t *testing.T) {
-	str := `module, Xauto_vector, 0.014188, 0.01378`
+func TestOne(t *testing.T) {
+	str := `[ { "sel": {"module": "ip73"}, "rpavf": 0.005, "wpavf": 1.0, "comment": "this" } ]`
 	a := Load(strings.NewReader(str))
 	if len(a) != 1 {
-		t.Errorf("Expecting to get a slice with 1 AceStruct. Got %d", len(a))
+		t.Errorf("Expecting a slice with 1 AceStruct. Got %d", len(a))
 	}
 }
 
-func TestMultiline(t *testing.T) {
-	str := `  // Comment
-
-module, Xauto_vector_1,0.014188,0.01378
-
-	name,Xauto_vector_2,0.018747,0.01861
-
-  module,Xauto_vector_3,0.018747,0.01861
-	# another comment
-	`
+func TestTwo(t *testing.T) {
+	str := `[
+	{ "sel": {"module": "ip73"}, "rpavf": 0.005, "wpavf": 1.0, "comment": "this" },
+	{
+		"sel": {"module": "ip73", "name": "abc", "comment": "this is a comment"},
+		"rpavf": 0.005,
+		"wpavf": 1.0,
+		"comment": "this"
+	}
+]`
 	a := Load(strings.NewReader(str))
-	if len(a) != 3 {
-		t.Errorf("Expecting to get a slice with 3 AceStruct. Got %d", len(a))
+	if len(a) != 2 {
+		t.Errorf("Expecting a slice with 2 AceStruct. Got %d", len(a))
+	}
+}
+
+func TestThree(t *testing.T) {
+	str := `
+[
+
+{
+	"sel": {
+		"module": "parcpmssmb\/Xcpmssmb\/Xpke\/Xu_pke_unit128_rf_mems\/Xpke_unit128_wrap_mem_pke_banka0_mem_shell_128x72\/Xram_row_0_col_0\/Xip743rfshpm1r1w128x72c1p1$",
+		"name": "idinp0"
+	},
+	"rpavf": 0.0,
+	"wpavf": 0.0,
+	"comment": "Ignore pke slices other than 0"
+},
+
+{
+	"sel": {
+		"module": "parcpmssmb\/Xcpmssmb\/Xpke\/Xu_pke_unit128_rf_mems\/Xpke_unit128_wrap_mem_pke_banka0_mem_shell_128x72\/Xram_row_0_col_0\/Xip743rfshpm1r1w128x72c1p1$",
+		"name": "odoutp0"
+	},
+	"rpavf": 0.0,
+	"wpavf": 0.0,
+	"comment": "Ignore pke slices other than 0"
+}
+
+]
+`
+
+	a := Load(strings.NewReader(str))
+	if len(a) != 2 {
+		t.Errorf("Expecting a slice with 2 AceStruct. Got %d", len(a))
 	}
 }
