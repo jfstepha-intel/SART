5c06c8e6a9d27e34c61f5da6e812d3e859972edc
ngeorge3 <nishant.george@intel.com>
2019-06-20 14:12:56 -0700

    Initial revision

---
+++ cmd/tree/tree.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"sart/rtl"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var indent string = "|   "
+
+func Tree(prefix string, level int, top string) {
+	if upto > 0 && level > upto {
+		return
+	}
+
+	log.Printf("%s%s", prefix, top)
+	q := session.DB("sart").C(cache + "_insts").Find(bson.M{"module": top})
+
+	iter := q.Iter()
+
+	var inst bson.M
+	for iter.Next(&inst) {
+		if !inst["isprim"].(bool) {
+			Tree(prefix+indent, level+1, inst["type"].(string))
+		}
+	}
+}
+
+var server, cache, top string
+var session *mgo.Session
+var upto int
+
+func main() {
+	flag.StringVar(&server, "server", "localhost", "name of mongodb server")
+	flag.StringVar(&cache, "cache", "", "name of cache to save module info")
+	flag.StringVar(&top, "top", "", "name of top cell to explore")
+	flag.IntVar(&upto, "upto", 1, "depth to which hierarchy is sought. -1 for full hierarchy")
+
+	flag.Parse()
+
+	log.SetFlags(log.Lshortfile)
+	log.SetFlags(0)
+
+	if cache == "" || top == "" {
+		flag.PrintDefaults()
+		log.Fatal("Insufficient arguments")
+	}
+
+	var err error
+	session, err = mgo.Dial(server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rtl.InitMgo(session, cache, false)
+
+	log.SetOutput(os.Stdout)
+
+	Tree("", 0, top)
+}
