25483c68c88cb8982ed5b76a177bff0495b16569
ngeorge3 <nishant.george@intel.com>
2021-04-28 09:20:23 -0700

    Print instance name as well.

--- cmd/tree/tree.go
+++ cmd/tree/tree.go
@@ -12,12 +12,12 @@ import (
 
 var indent string = "|   "
 
-func Tree(prefix string, level int, top string) {
+func Tree(prefix string, level int, top, name string) {
 	if upto > 0 && level > upto {
 		return
 	}
 
-	log.Printf("%s%s", prefix, top)
+	log.Printf("%s%s (%s)", prefix, top, name)
 	q := session.DB("sart").C(cache + "_insts").Find(bson.M{"module": top})
 
 	iter := q.Iter()
@@ -25,7 +25,7 @@ func Tree(prefix string, level int, top string) {
 	var inst bson.M
 	for iter.Next(&inst) {
 		if !inst["isprim"].(bool) {
-			Tree(prefix+indent, level+1, inst["type"].(string))
+			Tree(prefix+indent, level+1, inst["type"].(string), inst["name"].(string))
 		}
 	}
 }
@@ -36,8 +36,8 @@ var upto int
 
 func main() {
 	flag.StringVar(&server, "server", "localhost", "name of mongodb server")
-	flag.StringVar(&cache, "cache", "", "name of cache to save module info")
-	flag.StringVar(&top, "top", "", "name of top cell to explore")
+	flag.StringVar(&cache, "cache", "", "name of cache to save module info (req.)")
+	flag.StringVar(&top, "top", "", "name of top cell to explore (req.)")
 	flag.IntVar(&upto, "upto", 1, "depth to which hierarchy is sought. -1 for full hierarchy")
 
 	flag.Parse()
@@ -59,5 +59,5 @@ func main() {
 
 	log.SetOutput(os.Stdout)
 
-	Tree("", 0, top)
+	Tree("", 0, top, top)
 }
